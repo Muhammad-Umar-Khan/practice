0dd19dc5f385ca92a36e43e969d36e82568cdb5a
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-12-08 19:33:50 +0500

    basic hashmap

---
+++ hashMap.js
@@ -0,0 +1,23 @@
+// Create an empty hashmap
+let myMap = {};
+
+// Add key-value pairs
+myMap['key1'] = 'value1';
+myMap['key2'] = 'value2';
+myMap['key3'] = 'value3';
+
+// Access values using keys
+console.log(myMap['key1']); // Output: value1
+console.log(myMap['key2']); // Output: value2
+
+// Check if a key exists
+console.log('key3' in myMap); // Output: true
+console.log('key4' in myMap); // Output: false
+
+// Remove a key-value pair
+delete myMap['key2'];
+
+// Iterate over keys
+for (let key in myMap) {
+  console.log(key, myMap[key]);
+}
