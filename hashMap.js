2724284d99f2ddb31a4c7644830b9311eafc7a61
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-12-16 16:45:08 +0500

    run

--- hashMap.js
+++ hashMap.js
@@ -8,7 +8,6 @@ myMap["key3"] = "value3";
 console.log(myMap["key1"]); // Output: value1
 console.log(myMap["key2"]); // Output: value2
 
-// Check for key exists
 console.log("key3" in myMap); // Output: true
 console.log("key4" in myMap); // Output: false
 
