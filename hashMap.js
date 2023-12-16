badc4bc54bd5e878d27b3c52cbf1d695ad2e34b4
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-12-16 16:48:41 +0500

    output

--- hashMap.js
+++ hashMap.js
@@ -6,9 +6,9 @@ myMap["key2"] = "value2";
 myMap["key3"] = "value3";
 
 console.log(myMap["key1"]);
-console.log(myMap["key2"]); // Output: value2
+console.log(myMap["key2"]);
 
-console.log("key3" in myMap); // Output: true
+console.log("key3" in myMap);
 console.log("key4" in myMap); // Output: false
 
 // Remove a key-value pair
