ae99638a955633e4e093341f66ae2bb4b239bcfe
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-12-28 19:19:03 +0500

    items

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 let names = ["Umar", "Khan", "Mardan"];
 for (let name of names) {
-  console.log("Current item is", name);
+  console.log("Current item", name);
 }
 
 console.log(names.forEach((name) => name.includes("a")));
