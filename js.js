edf5af2926ef2a2a358d9967dfc71a5d0368eab8
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-12-29 23:59:44 +0500

    comments

--- js.js
+++ js.js
@@ -7,16 +7,12 @@
 // let info = names.filter((name) => name.includes("a"));
 // console.log(info);
 function containsDuplicates(arr) {
-  // Using Set to check for uniqueness
   const uniqueSet = new Set(arr);
-  
-  // If the size of the Set is equal to the length of the array, there are no duplicates
-  // Otherwise, there are duplicates
   return uniqueSet.size !== arr.length;
 }
 
 const numbersWithDuplicates = [1, 2, 3, 4, 5, 2];
 const uniqueNumbers = [1, 2, 3, 4, 5];
 
-console.log(containsDuplicates(numbersWithDuplicates));  // Output: true
-console.log(containsDuplicates(uniqueNumbers)); 
+console.log(containsDuplicates(numbersWithDuplicates)); // Output: true
+console.log(containsDuplicates(uniqueNumbers));
