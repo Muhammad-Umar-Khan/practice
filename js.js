6d2d83f95f2a77d3f44082837e8c208efcbe47bd
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-12-31 22:40:20 +0500

    comments

--- js.js
+++ js.js
@@ -1,13 +1,14 @@
-// let names = ["Umar", "Khan", "Mardan"];
-// for (let name of names) {
-//   console.log("Current item", name);
-// }
+let names = ["Umar", "Khan", "Mardan"];
+for (let name of names) {
+  console.log("Current item", name);
+}
 
-// console.log(names.forEach((name) => name.includes("a")));
-// let info = names.filter((name) => name.includes("a"));
-// console.log(info);
+console.log(names.forEach((name) => name.includes("a")));
+let info = names.filter((name) => name.includes("a"));
+console.log(info);
 function containsDuplicates(arr) {
   const uniqueSet = new Set(arr);
+  console.log("Set is", uniqueSet);
   return uniqueSet.size !== arr.length;
 }
 
