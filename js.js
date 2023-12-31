f070e0ce6d9bfcf384be76e529852ef7ee53523f
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-12-31 22:44:40 +0500

    console

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 // let names = ["Umar", "Khan", "Mardan"];
 // for (let name of names) {
 //   console.log("Current item", name);
-}
+// }
 
 // console.log(names.forEach((name) => name.includes("a")));
 // let info = names.filter((name) => name.includes("a"));
