0e9567d35bdc454747e8f69d81bd72144fdf1d35
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-01 13:19:10 +0500

    >0

--- reverse.js
+++ reverse.js
@@ -1,6 +1,6 @@
 function reverse(number) {
   let finalOutput = 0;
-  while (number !== 0) {
+  while (number > 0) {
     let lastInt = Math.floor(number % 10);
     number = Math.floor(number / 10);
     finalOutput = Math.floor(finalOutput * 10 + lastInt);
