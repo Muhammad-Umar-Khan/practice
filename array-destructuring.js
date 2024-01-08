84172590f19e711ee54da7de45f80ded081babf3
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-08 20:45:02 +0500

    skiping an element

--- array-destructuring.js
+++ array-destructuring.js
@@ -1,3 +1,3 @@
 const students = ["Alex", "Bob", "Charlie", "Jushua"];
-const [first] = students;
-console.log("First student is", first);
+const [first, ,third] = students;
+console.log("First student is", first, "third student is", third);
