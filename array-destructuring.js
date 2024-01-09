140a663ba09fa5397a21d47d6ac58129aff06ac4
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-09 18:40:20 +0500

    console

--- array-destructuring.js
+++ array-destructuring.js
@@ -1,3 +1,3 @@
 const students = ["Alex", "Bob", "Charlie", "Jushua"];
-const [first, , third] = students;
-console.log("First student is", first);
+const [first, ,third] = students;
+console.log("First student is", first, "third student is");
