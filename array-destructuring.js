2083cd42af286c89167930247397ad5be220228c
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-11 12:31:03 +0500

    replace with Umar

--- array-destructuring.js
+++ array-destructuring.js
@@ -3,4 +3,4 @@ const [first, , third, fourth] = students;
 console.log("First student is", first, "and fourth element");
 const copy = students.slice(0, 2);
 const SPLICE = students.splice(0, 1, "Umar");
-console.log(copy);
+console.log(copy, SPLICE);
