ec0c9288cbd6d1535334b51ff6e7d69de495a781
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-11 12:30:15 +0500

    splice

--- array-destructuring.js
+++ array-destructuring.js
@@ -2,4 +2,5 @@ const students = ["Alex", "Bob", "Charlie", "Jushua"];
 const [first, , third, fourth] = students;
 console.log("First student is", first, "and fourth element");
 const copy = students.slice(0, 2);
+const SPLICE = students.splice(0, 1, "Umar");
 console.log(copy);
