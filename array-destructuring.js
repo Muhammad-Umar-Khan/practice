1b42f6d297ebb77eb2d3f50a2293068f06a5759d
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-11 12:28:37 +0500

    [Alice, Bob]

--- array-destructuring.js
+++ array-destructuring.js
@@ -2,3 +2,4 @@ const students = ["Alex", "Bob", "Charlie", "Jushua"];
 const [first, , third, fourth] = students;
 console.log("First student is", first, "and fourth element");
 const copy = students.slice(0, 2);
+console.log(copy);
