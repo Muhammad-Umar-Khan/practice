2d6d066b2485eb428fc32124078b2c16f3c742f0
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-12 20:27:39 +0500

    includes

--- array-destructuring.js
+++ array-destructuring.js
@@ -4,3 +4,4 @@ console.log("First student is", first, "and fourth element");
 const copy = students.slice(0, 2);
 const SPLICE = students.splice(0, 1, "Umar");
 console.log(SPLICE);
+const arrayHas = students.includes("Alex");
