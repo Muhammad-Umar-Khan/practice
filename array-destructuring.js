f23df5b33d68cfd5dc060f7a00e6c5322b312745
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-01-13 16:55:22 +0500

    initial Value

--- array-destructuring.js
+++ array-destructuring.js
@@ -7,4 +7,5 @@ const SPLICE = students.splice(0, 1, "Umar");
 console.log(SPLICE);
 console.log("Array had Alex", arrayHas);
 const nums = [12, 23, 34, 45, 45];
-const sum = nums.reduce((accum, currentValue) => accum + currentValue, 23);
+const sum = nums.reduce((accum, currentValue) => accum + currentValue, 0);
+console.log("Sum of the array is", sum);
