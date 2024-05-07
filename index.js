a77a029c57c8d76a10f623c4cac3b48802187573
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2024-05-07 14:14:27 +0500

    speech

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-function factorial(n){
-    if(n === 0 || n === 1){
-        return 1;
-    }
-    return n * factorial(n-1);
-}
+// function factorial(n){
+//     if(n === 0 || n === 1){
+//         return 1;
+//     }
+//     return n * factorial(n-1);
+// }
 
-console.log(factorial(5));
+// console.log(factorial(5));
 
-console.log("Hello world with a new perspective")
+// console.log("Hello world with a new perspective")
+
+const currentTime = new Date();
+const fiveMinutesAgo = new Date(currentTime.getTime() - 5 * 60 * 1000);
+const unixTimestamp = Math.floor(fiveMinutesAgo.getTime() / 1000);
+console.log(unixTimestamp);
